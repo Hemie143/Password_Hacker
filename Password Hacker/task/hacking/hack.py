61b79fa7eff63559644095ba3750ea9339e82994
hemie143 <hemie143@gmail.com>
2020-08-16 08:55:45 +0200

    Stage3

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import string
 import itertools
+import urllib.request
 
 
 symbols = string.ascii_lowercase + string.digits
@@ -9,14 +10,19 @@ args = sys.argv
 ip_address = args[1]
 port = int(args[2])
 
+url = 'https://stepik.org/media/attachments/lesson/255258/passwords.txt'
+with urllib.request.urlopen(url) as f:
+    passwords = f.read().decode('utf-8')
+
+password_list = passwords.splitlines()
+
 with socket.socket() as client_socket:
     address = (ip_address, port)
     client_socket.connect(address)
-    n = 1
-    while True:
-        for p in itertools.product(symbols, repeat=n):
-            assert len(p) > 0
-            password = ''.join(list(p))
+    for word in password_list:
+        words_combo = itertools.product(*((c.lower(), c.upper()) for c in word))
+        for w in words_combo:
+            password = ''.join(w)
             data = password.encode()
             client_socket.send(data)
             response = client_socket.recv(1024)
@@ -28,4 +34,3 @@ with socket.socket() as client_socket:
                 exit()
             elif response == "Too many attempts":
                 exit()
-        n += 1
